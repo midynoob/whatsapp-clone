9b343e66550a1bf3e30a9ef40570fdecd47c4c45
midynoob <toufiqshaikh176@gmail.com>
2021-05-29 00:54:15 +0530

    Pre firebase deploy

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect } from 'react';
+import {Avatar, IconButton} from '@material-ui/core';
+import { DonutLarge, Chat, MoreVert, SearchOutlined } from '@material-ui/icons';
+import './Sidebar.css'
+import SidebarChat from '../SidebarChat/SidebarChat';
+import db from '../../firebase';
+import { useStateValue } from '../../StateProvider';
+
+const Sidebar = () => {
+
+    const [rooms, setRooms] = useState([]);
+    const [{user} , dispatch ] = useStateValue();
+
+    useEffect(() => {
+        const unsubscribe = db.collection('rooms').onSnapshot(snapshot => (
+            setRooms(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data(),
+            })
+            ))
+        ));
+        return () => {
+            unsubscribe();
+        }
+    }, []);
+
+    return (
+        <div className='sidebar' >
+            <div className="sidebar__header">   
+                <Avatar src={user?.photoURL} />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <Chat />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                    
+                    
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input type="text" placeholder="Search or Start new Chat" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat addNewChat />
+                {rooms.map(room => (
+                    <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar;
